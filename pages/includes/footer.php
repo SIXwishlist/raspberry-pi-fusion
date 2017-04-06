daa35fd958c5edaab1a93078a8c8cc04a779cdcc
Andreas Potthoff <mail@electrodrome.net>
2017-04-06 19:40:47 +0200

    Includes

---
+++ pages/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version </b> <?php echo (file_get_contents('../VERSION')); ?>
+    </div>
+    <strong>Pi Fusion</strong> &copy; by <a target="_blank" href="http://electrodrome.net">Andreas Potthoff</a> and <a href="../pages/pif_credits.php">contributors</a> - 2016-2017. Pi Fusion is licensed under GPL v2.0. Raspberry Pi is a trademark of the <a target="_blank" href="https://www.raspberrypi.org/">Raspberry Pi Foundation</a>.
+  </footer>
