8737525dc3626ce24f417569049d0becc7d53a90
Andreas Potthoff <mail@electrodrome.net>
2017-04-12 13:48:20 +0200

    Update

--- lib/actions/clean_cache.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "/var/www/html/fusion/lib/functions.php";
-$configArray = read_Settings();
-$connection = ssh2_connect($configArray ['ssh_cmd_ip'], 22);
-ssh2_auth_password($connection, $configArray ['ssh_cmd_user'], $configArray ['ssh_cmd_password']);
-$stream = ssh2_exec($connection, 'sync ; echo 3 | sudo tee /proc/sys/vm/drop_caches');
-?>
